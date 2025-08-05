c2374fc5a47671a84c6c42fa50c57ad6b3ac933c
Hugo Verly <verlyhugo959@gmail.com>
2025-08-05 12:46:33 +0900

    checkout page

--- restaurant-webapp/src/pages/Checkout.jsx
+++ restaurant-webapp/src/pages/Checkout.jsx
@@ -1,14 +1,27 @@
 import { useNavigate } from 'react-router-dom';
+import cardIcon from '../assets/credit-card-pay-svgrepo-com.svg';
+import cashIcon from '../assets/cash-svgrepo-com.svg';
 
 function Checkout() {
   const navigate = useNavigate();
 
   return (
-    <div className="centered">
-      <h1>Checkout</h1>
-      <button onClick={() => alert('Stripe flow soon...')}>Payer avec Stripe</button>
-      <button onClick={() => alert('QR Code généré')}>Paiement en cash</button>
-      <button onClick={() => navigate('/menu')}>Retour</button>
+    <div className="checkout-container">
+      <h1 className="checkout-title">Choisir méthode de paiement</h1>
+
+      <div className="payment-options">
+        <button className="payment-btn" onClick={() => alert('Stripe flow soon...')}>
+          <img src={cardIcon} alt="Carte bancaire" className="payment-icon"/>
+          <span>Carte bancaire</span>
+        </button>
+
+        <button className="payment-btn" onClick={() => alert('QR Code généré')}>
+          <img src={cashIcon} alt="Espèces" className="payment-icon" />
+          <span>Cash</span>
+        </button>
+      </div>
+
+      <button className="back-btn" onClick={() => navigate('/menu')}>Retour</button>
     </div>
   );
 }
